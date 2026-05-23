b1971e9aae749266f9c46129ec28ab819bb64f95
jaydyntfavors-tech <jaydyn.t.favors@gmail.com>
2026-05-23 18:43:26 -0400

    first bot

---
+++ test_bot.py
@@ -0,0 +1,6 @@
+import os
+from google import genai
+client = genai.Client()
+response = client.model.generate_content(model = 'gemini-1.5-flash',contents='Give me a 1 sentence tip on how to save money on groceries)
+print(response.text)                                         
+                                        
